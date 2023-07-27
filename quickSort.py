b8d27cf2c312041e5feeb95bad638150404fd17d
seungchan baek <poketsc@gmail.com>
2023-07-27 20:10:02 +0900

    퀵 정렬

---
+++ quickSort.py
@@ -0,0 +1,24 @@
+# 퀵 정렬
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left <= right:
+
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+
+        while right > start and array[pivot] <= array[right]:
+            right -= 1
+
+        if left > right:
+            array[pivot], array[right] = array[right], array[pivot]
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
